1a2790509b93e9ce053f4fdcd841d46a0c2188b8
Lokideos <archmage09@gmail.com>
2018-12-12 15:18:16 +0300

    Logger now show query string

--- middleware/logger.rb
+++ middleware/logger.rb
@@ -14,28 +14,12 @@ class ApplicationLogger
 
   def format_message(env)
     @logger.info("\nRequest: #{env['REQUEST_METHOD']} " +
-                 "#{env["simpler.controller"].request.env['REQUEST_PATH']}\n" +
+                 "#{env["simpler.controller"].request.env['REQUEST_PATH']}" +
+                 "?#{env['QUERY_STRING']}\n" +
                  "Handler: #{env['simpler.controller'].class}##{env['simpler.action']}\n" +
                  "Parameters: #{env["simpler.controller"].request.params}\n" +
                  "Response: #{env["simpler.controller"].response.status} " +
                  "[#{env["simpler.controller"].response.header['Content-Type']}] " +
                  "#{env['simpler.template_path']}")
-  end
-
-  def log_handler(handler, action)
-    @logger.info("Handler: #{handler.class}##{action}")
-  end
-
-  def log_params(params)
-    @logger.info("Parameters: #{params}")
-  end
-
-  def log_response(response)
-    @logger.info("Response: status #{response.status} " +
-                           "response-type #{response.header["Content-Type"]} ")
-  end
-
-  def log_response_body(body)
-    @logger.info("Response: response-body #{body}")
   end
 end
